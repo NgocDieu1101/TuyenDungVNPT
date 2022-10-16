b7daab8c3004e86980333ffbcf0bb57868694d7c
DieuNguyen <dieu13857e.comgit config --global user.email dieu13857@gmail.com>
2022-10-16 22:02:14 +0700

    update

--- asset/index.js
+++ asset/index.js
@@ -31,4 +31,13 @@ $(document).on('ready', function () {
       videoIframe.addClass('show');
     }
   })
+  $('#demo').pagination({
+    dataSource: [1, 2, 3, 4, 5, 6, 7, ... 195],
+    callback: function(data, pagination) {
+        // template method of yourself
+        var html = template(data);
+        dataContainer.html(html);
+    }
+})
 });
+
